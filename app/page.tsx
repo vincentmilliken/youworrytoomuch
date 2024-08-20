508250c6a418b3048f2499a384d678693bd07fda
Vincent Milliken <vincentmilliken@Vincents-MBP-2.Home>
2024-08-20 23:24:13 +0100

    adding some fun stuff

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex w-full min-h-screen flex-col items-center justify-between max-sm:mt-24 mb-24">
+      
       <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed block px-8 left-0 top-0 w-full max-w-5xl flex mx-auto border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
          The lads agency 
@@ -12,6 +13,14 @@ export default function Home() {
           
         </div>
       </div>
+
+      <div className="mb-15 lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+    <div className="z-10 max-w-5xl items-center font-mono text-sm lg:flex">
+      <p className="fixed block px-8 block left-0 bottom-0 flex w-full max-w-5xl border-b border-blue-300 bg-gradient-to-b from-blue-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-blue-800/30">
+         from the lads Vincent & Will
+        </p>
+      </div>
+    </div>
 
       <div className="flex place-items-center  before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
   
@@ -33,19 +42,17 @@ export default function Home() {
         </a> 
       </div>
 
-      <div className="mb-15 lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        
-    <div className="z-10 max-w-5xl items-center font-mono text-sm lg:flex">
 
-      <p className="fixed block px-8 block left-0 bottom-0 flex w-full max-w-5xl border-b border-blue-300 bg-gradient-to-b from-blue-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-blue-800/30">
-         The lads agency from Vincent & Will
-        </p>
-        <p className="py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
-        Coming very soon!</p>
-        </div>
 
-        
-      </div>
+
+<div className="overflow-hidden p-4 border-gray-300 bg-gradient-to-b from-zinc-200  backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+  <div className="marquee-content flex gap-4 items-center animate-marquee">
+    <div className="px-4 py-2 bg-blue-500 text-white rounded w-full">Best shit ever</div>
+    <div className="px-4 py-2 bg-green-500 text-white rounded w-full">Fuck up your competitors</div>
+    <div className="px-4 py-2 bg-cyan-500 text-white rounded w-full">Get access to the best lads in tech</div>
+    <div className="px-4 py-2 bg-yellow-500 text-black rounded w-full">BUFU</div>
+  </div>
+</div>
 
 <div className="grid gap-6 mt-6 md:gap-12 md:grid-cols-2 mx-auto ">   
 
