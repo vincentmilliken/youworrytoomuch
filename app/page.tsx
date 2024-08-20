ace6998c65ac3ccd689d5e992db0ae2da0fe0259
Vincent Milliken <vincentmilliken@Vincents-MBP-2.Home>
2024-08-20 21:39:04 +0100

    fixing errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col items-center justify-between max-sm:mt-24">
+    <main className="flex w-full min-h-screen flex-col items-center justify-between max-sm:mt-24 mb-24">
       <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed block px-8 left-0 top-0 w-full max-w-5xl flex mx-auto border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
          The lads agency 
@@ -40,54 +40,54 @@ export default function Home() {
       <p className="fixed block px-8 block left-0 bottom-0 flex w-full max-w-5xl border-b border-blue-300 bg-gradient-to-b from-blue-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-blue-800/30">
          The lads agency from Vincent & Will
         </p>
-        <p class="py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
+        <p className="py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
         Coming very soon!</p>
         </div>
 
         
       </div>
 
-<div class="grid gap-6 mt-6 md:gap-12 md:grid-cols-2 mx-auto ">   
+<div className="grid gap-6 mt-6 md:gap-12 md:grid-cols-2 mx-auto ">   
 
-<div class="max-w-sm  flex flex-col rounded-3xl bg-white dark:bg-gray-950 shadow-xl ring-1 ring-black/10">
-    <div class="p-8 sm:p-10">
-        <h3 id="tier-basic" class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Product & Design support</h3>
-        <div class="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+<div className="max-w-sm  flex flex-col rounded-3xl bg-white dark:bg-gray-950 shadow-xl ring-1 ring-black/10">
+    <div className="p-8 sm:p-10">
+        <h3 id="tier-basic" className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Product & Design support</h3>
+        <div className="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
             <span> €6.490 </span>
-            <span class="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
+            <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
         </div>
-        <p class="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
+        <p className="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
         Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so dont worry about it. 
 
         </p>
     </div>
-    <div class="flex flex-1 flex-col p-2">
-        <div class="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 dark:bg-gray-900 p-6 sm:p-8">
-            <ul role="list" class="space-y-6">
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+    <div className="flex flex-1 flex-col p-2">
+        <div className="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 dark:bg-gray-900 p-6 sm:p-8">
+            <ul role="list" className="space-y-6">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                         Support fom Director level Product expert (Meta, Booking.com & Uber) and Senior Designer (Booking.com, Uber & Airbnb)
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Landing pages, Web apps & Mobile apps
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Data Analytics streamlining (North Star, KPI's & Success metrics review)
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Conversion Rate Optimization (Let us create huge impact in increasing revenue with our 10+ years experience  )
                     </p>
                 </li>
             </ul>
-            <div class="mt-8">
+            <div className="mt-8">
                 <a href="#" aria-describedby="tier-basic"
-                    class="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700">
+                    className="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700">
                     Get started today
                 </a>
             </div>
@@ -95,47 +95,47 @@ export default function Home() {
     </div>
 </div>
 
-<div class="max-w-sm relative flex flex-col rounded-3xl bg-white dark:bg-gray-950 shadow-xl ring-1 ring-black/10 bg-white/5 ring-2 ring-indigo-500 rounded-3xl">
-    <div class="p-8 sm:p-10">
-        <h3 id="tier-basic" class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Product & Design support</h3>
-        <p class="absolute top-0 py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
+<div className="max-w-sm relative flex flex-col rounded-3xl bg-white dark:bg-gray-950 shadow-xl ring-1 ring-black/10 bg-white/5 ring-2 ring-indigo-500 rounded-3xl">
+    <div className="p-8 sm:p-10">
+        <h3 id="tier-basic" className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Product & Design support</h3>
+        <p className="absolute top-0 py-1.5 px-4 bg-emerald-500 text-white rounded-full text-xs font-semibold uppercase tracking-wide  transform -translate-y-1/2">
                     Most popular</p>
-        <div class="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+        <div className="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
             <span> €10.690 </span>
-            <span class="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
+            <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
         </div>
-        <p class="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
+        <p className="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
         Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so dont worry about it. 
 
         </p>
     </div>
-    <div class="flex flex-1 flex-col p-2">
-        <div class="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 dark:bg-gray-900 p-6 sm:p-8">
-            <ul role="list" class="space-y-6">
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+    <div className="flex flex-1 flex-col p-2">
+        <div className="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 dark:bg-gray-900 p-6 sm:p-8">
+            <ul role="list" className="space-y-6">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                         Support fom Director level Product expert (Meta, Booking.com & Uber) and Senior Designer (Booking.com, Uber & Airbnb)
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Landing pages, Web apps & Mobile apps
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Data Analytics streamlining (North Star, KPI's & Success metrics review)
                     </p>
                 </li>
-                <li class="flex items-start">
-                    <p class="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+                <li className="flex items-start">
+                    <p className="ml-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
                     Conversion Rate Optimization (Let us create huge impact in increasing revenue with our 10+ years experience  )
                     </p>
                 </li>
             </ul>
-            <div class="mt-8">
+            <div className="mt-8">
                 <a href="#" aria-describedby="tier-basic"
-                    class="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700">
+                    className="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700">
                     Get started today
                 </a>
             </div>
