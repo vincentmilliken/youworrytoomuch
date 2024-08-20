f14cc73d9e1a76f935aa8c71c185a89e9d083022
Vincent Milliken <vincentmilliken@Vincents-MBP-2.Home>
2024-08-20 19:55:44 +0100

    some more content

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         The lads agancy 
+         The lads agency 
           <code className="font-mono font-bold"> - We will worry about it</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -35,9 +35,12 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 
-       
-
+      <p className="fixed left-0 bottom-0 flex w-full justify-center border-b border-blue-300 bg-gradient-to-b from-blue-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-blue-800/30">
+         The lads agency from Vincent & Will <code className="font-mono font-bold"> - Comming soon.</code>
+        </p>
+        </div>
       
 
         
