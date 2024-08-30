1597e028a46b5e17008f85de0926bd8b0683f3d0
Vincent <vincentamilliken@gmail.com>
2024-08-30 15:49:37 +0100

    Update copy with some small changes

--- app/page.tsx
+++ app/page.tsx
@@ -28,26 +28,18 @@ export default function Home() {
     <div className="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-24">
       <div>
         <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-5xl text-balance">
-        Premium on-demand partnership that   
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#9867F0] to-[#ED4E50]"> grows your business.</span>
+        Strategic Design,    
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#9867F0] to-[#ED4E50]"> Tangible Results.</span>
         </h1>
          <p className="mt-4 text-base font-medium text-gray-500 text-balance"> 
-          The value that we will bring to your busines will increase your conversion rates and double your revenue.
-        </p>
-        <p className="mt-4 text-base font-medium text-gray-500 text-balance">
-         Our unique service will guarantee a increase your conversion rates and double your business.
+         Merging design and data to create compelling experiences that convert.
         </p>
 
         <div className="flex flex-col items-center gap-2 mx-auto mt-8 md:flex-row">
-                          <button className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black" aria-label="Primary action">
-                            See our pricing
-                          </button>
-                          <button className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" aria-label="Secondary action">
-                            Learn more
-                          </button>
-                        </div>
-
-
+              <button className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black" aria-label="Primary action">
+                See our pricing
+              </button>
+          </div>
       </div>
       <div className="flex flex-col gap-y-12 text-balance">
         <div className="flex flex-col gap-4 lg:flex-row">
@@ -56,9 +48,9 @@ export default function Home() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"></path></svg></span>
           </div>
           <div>
-            <h3 className="font-medium text-gray-900">Increase your conversion rates</h3>
+            <h3 className="font-medium text-gray-900">Increase your products conversion rates</h3>
             <p className="mt-2 text-sm text-gray-500">
-              Our high level experience with A/B testing, we know what will be successful for your businuess. 
+              Our high level experience with A/B testing, we know what will be successful for your business. 
             </p>
           </div>
         </div>
@@ -69,7 +61,7 @@ export default function Home() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"></path></svg></span>
           </div>
           <div>
-            <h3 className="font-medium text-gray-900">Specialised Design & Product</h3>
+            <h3 className="font-medium text-gray-900">Specialized combo of Design & Product</h3>
             <p className="mt-2 text-sm text-gray-500">
              Providing our Design & Product skill sets is a unique service that covers a wider range of impact.
             </p>
@@ -81,9 +73,9 @@ export default function Home() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"></path></svg></span>
           </div>
           <div>
-            <h3 className="font-medium text-gray-900">On-demand service & transparant pricing</h3>
+            <h3 className="font-medium text-gray-900">On-demand service & transparent pricing</h3>
             <p className="mt-2 text-sm text-gray-500">
-              More affordable than specialised agencys, Fast-Paced service that you can cancel at anytime. 
+              More affordable than specialized agency's, Fast-Paced service that you can cancel at anytime. 
             </p>
           </div>
         </div>
@@ -92,36 +84,7 @@ export default function Home() {
   </div>
 </section>
 
-
-              <section>
-                <div className="px-8 py-2 mx-auto md:px-12 lg:px-32 max-w-7xl">
-                  <div className="grid items-center grid-cols-1 py-12 lg:grid-cols-4 border-y md:grid-cols-2 border-onyx-800 lg:gap-24">
-                    <div className="mx-auto col-span-full lg:col-span-1 lg:max-w-none lg:mr-auto">
-                      <p className="text-xs text-gray-500">
-                        Some of the mazing companies we have worked with
-                      </p>
-                    </div>
-                    <div className="flex flex-wrap justify-between mt-12 lg:mt-0 md:col-span-3">
-                      <div className="flex justify-center col-span-1 px-8">
-                        <img className="max-h-12" src="https://windstatic.com/images/logos/1.svg" alt="logo" />
-                      </div>
-                      <div className="flex justify-center col-span-1 px-8">
-                        <img className="max-h-12" src="https://windstatic.com/images/logos/2.svg" alt="logo" />
-                      </div>
-                      <div className="flex justify-center col-span-1 px-8">
-                        <img className="max-h-12" src="https://windstatic.com/images/logos/3.svg" alt="logo" />
-                      </div>
-                      <div className="flex justify-center col-span-1 px-8">
-                        <img className="max-h-12" src="https://windstatic.com/images/logos/4.svg" alt="logo" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-
-
-
-<section>
+                <section>
                 <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
                   <div className="grid gap-12 lg:grid-cols-3">
                     <div>
@@ -215,7 +178,7 @@ export default function Home() {
                         <div>
                           <div>❖</div>
                           <dt className="mt-4 font-medium text-gray-900">
-                            Clear & transparant
+                            Clear & transparent
                           </dt>
                           <dd className="mt-2 text-sm text-gray-500">
                             This is placeholder text for this usp. Need to fill out and refine 
@@ -287,11 +250,11 @@ export default function Home() {
     <div className="px-4 py-2 bg-blue-500 text-white rounded w-full">Best shit ever</div>
     <div className="px-4 py-2 bg-green-500 text-white rounded w-full">Fuck up your competitors</div>
     <div className="px-4 py-2 bg-cyan-500 text-white rounded w-full">Get access to the best lads in tech</div>
-    <div className="px-4 py-2 bg-yellow-500 text-black rounded w-full">BUFU</div>
+    <div className="px-4 py-2 bg-yellow-500 text-black rounded w-full"> </div>
     <div className="px-4 py-2 bg-blue-500 text-white rounded w-full">Best shit ever</div>
     <div className="px-4 py-2 bg-green-500 text-white rounded w-full">Fuck up your competitors</div>
     <div className="px-4 py-2 bg-cyan-500 text-white rounded w-full">Get access to the best lads in tech</div>
-    <div className="px-4 py-2 bg-yellow-500 text-black rounded w-full">BUFU</div>
+    <div className="px-4 py-2 bg-yellow-500 text-black rounded w-full"> </div>
 
   </div>
 </div> */}
@@ -306,7 +269,7 @@ export default function Home() {
             <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
         </div>
         <p className="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
-        Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so dont worry about it. 
+        Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so don´t worry about it. 
 
         </p>
     </div>
@@ -354,7 +317,7 @@ export default function Home() {
             <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">/Per Month</span>
         </div>
         <p className="mt-6 text-base leading-7 text-gray-600 dark:text-slate-200">
-        Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so dont worry about it. 
+        Product & Design starting as soon as you sign up. No contracts or long onboarding process, real and impactful value, so don´t worry about it. 
 
         </p>
     </div>
