97c55095253cda3a364996dacc8d2c4905aa154d
Vincent <vincentamilliken@gmail.com>
2024-08-31 12:48:33 +0100

    adding header with name as example

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,45 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-
+      <header className="border-b-1 relative z-20 w-full border-b border-slate-200 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:left-0 after:top-full after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
+      <main className="flex w-full flex-col">
+        <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
+          <nav
+            aria-label="main navigation"
+            className="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700"
+            role="navigation"
+          >
+            {/*      <!-- Brand logo --> */}
+            <a
+              id="WindUI"
+              aria-label="WindUI logo"
+              aria-current="page"
+              className="flex items-center gap-2 whitespace-nowrap py-3 text-lg focus:outline-none lg:flex-1"
+              href="#"
+            >
+              EngageMint
+            </a>
+            {/*      <!-- Mobile trigger --> */}
+            {/*      <!-- Navigation links --> */}
+            <ul
+              role="menubar"
+              aria-label="Select page"
+              className="absolute left-0 top-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ">
+              {/* <li role="none" className="flex items-stretch">
+                <a
+                  role="menuitem"
+                  aria-haspopup="false"
+                  className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8"
+                  href="#"
+                >
+                  <span>About</span>
+                </a>
+              </li> */}
+            </ul>
+          </nav>
+        </div>
+        </main>
+      </header>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col items-center justify-between max-sm:mt-24 mb-24">
+   
+   <main className="flex w-full min-h-screen flex-col items-center justify-between max-sm:mt-24 mb-24">
       
       {/* <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed block px-8 left-0 top-0 w-full max-w-5xl flex mx-auto border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
